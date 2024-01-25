fa3c90cc7df9cf0cb7888109e05af04b5ee03bc4
Albin Tallkvist <albin.tallkvist@elev.ga.ntig.se>
2024-01-25 13:49:10 +0100

    RestingBFace

---
+++ RestingBFaceAlbin/Digimon.cs
@@ -0,0 +1,7 @@
+using System.Text.Json.Serialization;
+
+public class Digimon
+{
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+}
---
+++ RestingBFaceAlbin/Program.cs
@@ -0,0 +1,17 @@
+﻿using RestSharp;
+using System.Text.Json;
+using System.IO;
+
+RestClient klient = new("https://digi-api.com/api/v1/digimon/");
+string digimonNamn = "500";
+RestRequest begär = new($"{digimonNamn}");
+RestResponse svar = klient.GetAsync(begär).Result;
+
+Digimon d = JsonSerializer.Deserialize<Digimon>(svar.Content);
+
+System.Console.WriteLine(d.Name);
+File.WriteAllText("digimon.json", svar.Content);
+
+
+Console.ReadLine();
+
